6a0a990b2a7a487f317b73e6fbd2edd2de54ce59
bradg4508 <64989476+bradg4508@users.noreply.github.com>
2023-03-14 11:35:07 -0500

    Have each pass into square generate random color

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 // Etch-a-Sketch
 
+// Get a random color to set as the background color for each square as you enter it
+function getRandomColor() {
+    return Math.floor(Math.random() * 256);
+}
+
 // Set up a “hover” effect so that the grid divs change color when the mouse passes over them, 
 // It will leave a (pixelated) trail through the grid like a pen would
-function hover () {
+function hover() {
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
         square.addEventListener("mouseenter", () => {
-            square.style.cssText = "background-color: #868e96";
+            square.style.cssText = `background: rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`;
         });
     });
 }
 
 // Create a webpage with a grid of square divs
 // Create the divs using JavaScript if the user enters a valid grid number
-function makeNewGrid () {
+function makeNewGrid() {
     const div = document.querySelector("div.container");
     let gridSize = +prompt("How many squares per side would you like for your grid?\nEnter an integer between 1 and 100");
     while (!Number.isInteger(gridSize) || (gridSize < 1 && gridSize !== 0)|| gridSize > 100) {
